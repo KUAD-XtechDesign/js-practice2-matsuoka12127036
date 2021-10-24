15cf870b065b09319eaf40233a99068cfcecbc37
matsuoka12127036 <a-matsuoka3278@st.kyoto-art.ac.jp>
2021-10-24 21:54:23 +0900

    　

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,6 +1,44 @@
 $(function(){
 
+    let txt1 = "おはようございます";
+    let txt2 = "お元気ですか？";
+    let num1 = 20;
+    let num2 = 32;
+
+    function sayHellow(){
+        $("#content").text("こんにちは！");
+    }
+
+    function saysomething(txt){
+        $("#content").text(txt1);
+    }
+
+    function samTwo(arg1,arg2){
+        $("#content").text(arg1+arg2);
+    }
 
 
+    $("#button01").on("click",function(){
+        sayHellow();
+    })
+    
+    $("#button02").on("click",function(){
+        saysomething(txt1);
+    })
+    
+    $("#button03").on("click",function(){
+        samTwo(txt1,txt2);
+    })
+    
+    $("#button04").on("click",function(){
+        samTwo(num1,num2);
+    })
 
-})
+    $("#button05").on("click",function(){
+        samTwo(txt1,txt2);
+    
+    })
+
+    
+    })
+    
--- sample/js/script02.js
+++ sample/js/script02.js
@@ -1,5 +1,38 @@
 $(function(){
+let hako=0;
 
+function checkbox(){
+   if (hako>5){
+       $("#content").text(hako+"個入っています。。箱はいっぱいです。箱を空にします。")
+       hako=0;
+   }
+   else if (hako <=0){
+       hako=0;
+       $("#content").text(hako+"個入っていません");
+   }
+   else{
+    $("#content").text(hako+"個入っています。まだ入ります。");
+   }
+}
 
+$("#button01").on("click",function(){
+    sayHellow();
+})
 
+$("#button02").on("click",function(){
+    saysomething(txt1);
+})
+
+$("#button03").on("click",function(){
+    samTwo(txt1,txt2);
+})
+
+$("#button04").on("click",function(){
+    samTwo(num1,num2);
+})
+
+$("#button05").on("click",function(){
+    samTwo(txt1,txt2);
+
+})
 })
