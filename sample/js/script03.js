d798dd22da375c6d8fccd6b5ee92fef35be10150
matsuoka12127036 <a-matsuoka3278@st.kyoto-art.ac.jp>
2021-10-29 15:34:01 +0900

    3

--- sample/js/script03.js
+++ sample/js/script03.js
@@ -1,5 +1,40 @@
 $(function(){
 
+    let hako=0;
 
-  
+    $("#button01").on("click",function(){
+        for(let i = 0; i < 10; i++){
+        hako=hako+2;
+    }
+
+    $("#content").text(hako)
+    })
+    
+
+    $("#button02").on("click",function(){
+        for(let i = 0; i < 10; i++){
+            hako=hako+i;
+    }
+    
+    $("#content").text(hako)
+})
+    
+let timer;
+
+
+    $("#button03").on("click",function(){
+        hako--;
+        checkbox();
+
+        $("#content").text(hako)
+    },10)s
+
+        $("#button04").on("click",function(){
+            hako = 0;
+
+            $("#content").text(hako)
+
+            clearInterval(timer)
+
+    })
 })
