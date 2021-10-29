ce694ef8e69b2d0ca46184f0ddbadd13efa47026
matsuoka12127036 <a-matsuoka3278@st.kyoto-art.ac.jp>
2021-10-29 14:09:51 +0900

    2

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -4,35 +4,31 @@ let hako=0;
 function checkbox(){
    if (hako>5){
        $("#content").text(hako+"個入っています。。箱はいっぱいです。箱を空にします。")
+       $("#content").removeClass().addClass('full')
        hako=0;
    }
    else if (hako <=0){
        hako=0;
        $("#content").text(hako+"個入っていません");
+       $("#content").removeClass().addClass('empty')
    }
    else{
     $("#content").text(hako+"個入っています。まだ入ります。");
+    $("#content").removeClass()
    }
 }
 
 $("#button01").on("click",function(){
-    sayHellow();
+    checkbox();
 })
 
 $("#button02").on("click",function(){
-    saysomething(txt1);
+    hako++;
+    checkbox();
 })
 
 $("#button03").on("click",function(){
-    samTwo(txt1,txt2);
-})
-
-$("#button04").on("click",function(){
-    samTwo(num1,num2);
-})
-
-$("#button05").on("click",function(){
-    samTwo(txt1,txt2);
-
+    hako--;
+    checkbox();
 })
 })
